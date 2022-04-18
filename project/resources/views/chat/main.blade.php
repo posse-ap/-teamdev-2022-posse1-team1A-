6ef1ca5dbdc2d166a5106dc205bcd9689eb569f9
tomoharuyuyama <tomoharuyuyama@gmail.com>
2022-04-18 22:50:24 +0900

    チャット画面フロントざっくり

--- project/resources/views/chat/main.blade.php
+++ project/resources/views/chat/main.blade.php
@@ -6,11 +6,14 @@
         body {
             background-color: rgb(226 232 240);
         }
+        .cards {
+          height: 57vh;
+        }
 
     </style>
-    <div class="wrapper container mx-auto px-4 mb-5 pb-5 bg-white h-screen font-normal">
+    <div class="wrapper container mx-auto pl-4 mb-0 pb-0 bg-white font-normal">
         <div class='flex flex-wrap'>
-            <div class="w-full lg:w-1/4 border-r border-zinc-300 h-screen">
+            <div class="w-full lg:w-1/4 border-r border-zinc-300">
                 <ul class="mt-5 pt-5">
                     <li class="mt-5 pt-5">チャット一覧に戻る ></li>
                     <li class="mt-3">トークを退出する ></li>
@@ -35,5 +38,106 @@
                       </button>
                     </div>
                 </div>
+                <div class="cards mx-3 overflow-scroll">
+                  <div class="card m-5 p-3 bg-amber-100 w-72 rounded-md drop-shadow-md ml-auto">
+                    日程が決まりましたら、<br>
+                    日程登録ボタンを押してください。<br>
+                    リマインドメールが送信されます。
+                  </div>
+                  <div class="card flex items-center h-max">
+                    <div class="icon w-14 h-14">
+                      <img src="{{ asset('img/menIcon.png') }}" alt="">
+                    </div>
+                    <div class="m-5 p-3 w-72 rounded-md border-2 rounded-full mr-1">
+                      よろしくお願いいたします！
+                    </div>
+                    <div class="time mt-auto mb-5">
+                      <span class="text-slate-400">13:00</span>
+                    </div>
+                  </div>
+                  <div class="card flex items-center justify-end">
+                    <div class="icon w-14 h-14">
+                      {{-- <img src="{{ asset('img/menIcon.png') }}" alt=""> --}}
+                    </div>
+                    <div class="time mt-auto mb-5">
+                      <span class="text-slate-400 block text-right">既読</span>
+                      <span class="text-slate-400 block">13:00</span>
+                    </div>
+                    <div class="m-5 p-3 w-72 bg-slate-100 rounded-full">
+                      よろしくお願いいたします！
+                    </div>
+                  </div>
+                  <div class="card flex items-center">
+                    <div class="icon w-14 h-14">
+                      {{-- <img src="{{ asset('img/menIcon.png') }}" alt=""> --}}
+                    </div>
+                    <div class="m-5 p-3 w-72 rounded-md border-2 rounded-full mr-1">
+                      よろしくお願いいたします！
+                    </div>
+                    <div class="time mt-auto mb-5">
+                      <span class="text-slate-400">13:00</span>
+                    </div>
+                  </div>
+                  <div class="card flex items-center">
+                    <div class="icon w-14 h-14">
+                      <img src="{{ asset('img/menIcon.png') }}" alt="">
+                    </div>
+                    <div class="m-5 p-3 w-72 rounded-md border-2 rounded-full mr-1">
+                      よろしくお願いいたします！！！！
+                    </div>
+                    <div class="time mt-auto mb-5">
+                      <span class="text-slate-400">13:00</span>
+                    </div>
+                  </div>
+                  <div class="card flex items-center">
+                    <div class="icon w-14 h-14">
+                      {{-- <img src="{{ asset('img/menIcon.png') }}" alt=""> --}}
+                    </div>
+                    <div class="m-5 p-3 w-72 rounded-md border-2 rounded-full mr-1 bg-sky-100">
+                      よろしくお願いいたします！！！！
+                    </div>
+                    <div class="time mt-auto mb-5">
+                      <span class="text-slate-400">13:00</span>
+                    </div>
+                  </div>
+                  <div class="card flex items-center">
+                    <div class="icon w-14 h-14">
+                      <img src="{{ asset('img/anovey.png') }}" alt="">
+                    </div>
+                    <div class="m-5 p-3 w-72 rounded-md border-2 rounded-full mr-1 bg-sky-100">
+                      よろしくお願いいたします！！！！
+                    </div>
+                    <div class="time mt-auto mb-5">
+                      <span class="text-slate-400">13:00</span>
+                    </div>
+                  </div>
+                  <div class="card flex items-center justify-end">
+                    <div class="icon w-14 h-14">
+                      {{-- <img src="{{ asset('img/menIcon.png') }}" alt=""> --}}
+                    </div>
+                    <div class="time mt-auto mb-5">
+                      <span class="text-slate-400 block text-right">既読</span>
+                      <span class="text-slate-400 block">13:00</span>
+                    </div>
+                    <div class="m-5 p-3 w-72 bg-slate-100 rounded-full">
+                      よろしくお願いいたします！
+                    </div>
+                  </div>
+                </div>
+                <div class="flex items-center ">
+                  <div class="m-5 p-3 bg-slate-100 rounded-full w-full">
+                    よろしくお願いいたします！
+                  </div>
+                  <div class="icon w-28">
+                    <button class="bg-gray-400 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">
+                      <a href="" class="px-3">
+                        送信
+                      </a>
+                    </button>
+                  </div>
+                </div>
+                {{-- <div class="cards mx-3">
+                </div> --}}
+                
             </div>
         @endsection
