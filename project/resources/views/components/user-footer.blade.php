e226d1a719d169839cb6b8b37c70797f6d199125
Haruka Miki <haru.m1k1@keio.jp>
2022-04-19 00:38:55 +0900

    footerレスポンシブ完成

--- project/resources/views/components/user-footer.blade.php
+++ project/resources/views/components/user-footer.blade.php
@@ -1,31 +1,36 @@
-<footer class="footer px-10 bg-gray-500 w-full h-16 px-10 sticky bottom-0">
-<div class="flex items-center justify-between flex-shrink-0 px-10">
-  <a class="" href="">
-    <div class="flex w-8 h-8 ml-0 mr-1">
-      <img src="img/logo-black.png" alt="logo-black.png">
-      <p class="flex ml-2 text-white items-center border-r pr-4">Anovey</p>
+<footer class="footer px-10 bg-gray-500 w-full px-10 py-5">
+  <div class="flex items-center justify-between flex-shrink-0">
+    <a class="" href="">
+      <div class=" w-8 h-8 ml-0 mr-1 w-3/12 flex">
+        <img src="img/logo-black.png" alt="logo-black.png">
+        <p class="flex ml-2 text-white items-center border-r pr-4 footer-logo">Anovey</p>
+      </div>
+    </a>
+    <div class="md:flex justify-between w-full">
+      <div class="flex justify-between md:w-full lg:w-6/12">
+        <a class="w-6/12" href="">
+          <p class=" flex text-white h-8 items-center text-sm hover:text-gray-800 justify-center">
+            利用規約
+          </p>
+        </a>
+        <a class="w-6/12" href="">
+          <p class=" flex text-white h-8 items-center text-sm hover:text-gray-800 justify-center">
+            個人情報保護方針
+          </p>
+        </a>
+      </div>
+      <div class="flex justify-between md:w-full lg:w-6/12">
+        <a class="w-6/12" href="">
+          <p class=" flex text-white h-8 items-center text-sm hover:text-gray-800 justify-center">
+            運営会社
+          </p>
+        </a>
+        <a class="w-6/12" href="">
+          <p class=" flex text-white h-8 items-center text-sm hover:text-gray-800 justify-center">
+          お問い合わせ
+        </p>
+      </a>
     </div>
-  </a>
-  <a class="" href="">
-    <p class="flex text-white h-8 ml-0 mr-1 items-center text-sm hover:text-gray-800">
-      利用規約
-    </p>
-  </a>
-  <a class="" href="">
-    <p class="flex text-white h-8 ml-0 mr-1 items-center text-sm hover:text-gray-800">
-      個人情報保護方針
-    </p>
-  </a>
-  <a class="" href="">
-    <p class="flex text-white h-8 ml-0 mr-1 items-center text-sm hover:text-gray-800">
-      運営会社
-    </p>
-  </a>
-  <a class="" href="">
-    <p class="flex text-white h-8 ml-0 mr-1 items-center text-sm hover:text-gray-800">
-      お問い合わせ
-    </p>
-  </a>
-  
-</div>
-</footer>
+  </div>
+  </div>
+  </footer>
--- project/resources/views/components/user-header.blade.php
+++ project/resources/views/components/user-header.blade.php
@@ -1,17 +1,17 @@
 {{-- ログインする前のもの --}}
 <header class="sticky top-0">
-	<div class="flex items-center justify-between  flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-75 shadow-lg">
+	<div class="flex items-center justify-between flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-75 shadow-lg">
     <div class="flex w-8 h-8 ml-0 mr-auto">
       <img src="img/logo-black.png" alt="logo-black.png">
       <p class="flex ml-2 items-center"><b>Anovey<b></p>
     </div>
 		<div class="flex flex-row-reverse ml-auto mr-0">
-      <button class="bg-blue-800 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded ml-2">
+      <button class="bg-blue-800 hover:bg-blue-700 text-white sm:text-base text-xs font-bold py-1 px-4 rounded ml-2">
         <a href="">
           新規登録
         </a>
       </button>
-      <button class="bg-gray-500 hover:bg-gray-800 text-white font-bold py-1 px-4 rounded ml-2">
+      <button class="bg-gray-500 hover:bg-gray-800 text-white sm:text-base text-xs font-bold py-1 px-4 rounded ml-2">
         <a href="">
           ログイン
         </a>
