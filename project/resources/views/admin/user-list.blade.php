16d2ef2e85783cbaf43320488072828f6e0826fb
Haruka Miki <haru.m1k1@keio.jp>
2022-04-28 00:20:52 +0900

    いったん提出

--- project/resources/views/admin/user-list.blade.php
+++ project/resources/views/admin/user-list.blade.php
@@ -9,82 +9,44 @@
     <title>ユーザー一覧</title>
 </head>
 
-<body>
-    <div class="flex h-screen">
-        <div class="w-1/5 bg-blue-800">
+<body class="bg-grey-100">
+    @include('components.admin-bar')
 
-            <ul class="flex-grow">
-                <div class="w-10 h-10 mt-3 flex ml-5 mr-auto">
-                    <img src="{{ asset('img/logo-white.png') }}" alt="logo-black.png">
-                    <p class="flex ml-0 text-sm text-white items-center"><b>Anovey<b></p>
-                </div>
-                <li class="mx-5 border-b"><a class="block py-5 pl-2 rounded text-white" href="#">管理者ページ</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">各種指標</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">ユーザー一覧</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">お問合せ内容</a></li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">通報一覧</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">通話評価</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">トーク評価</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">退会理由一覧</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">管理者ユーザー追加</a>
-                </li>
-                <li><a class="block ml-5 py-5 pl-2 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
-                        href="#">利用規約・各種フォーム設定</a>
-                </li>
-            </ul>
+    <div class="mx-5">
+        <h1 class="text-5xl py-12 text-blue-800">ユーザー一覧</h1>
+        <div class="block relative space-y-3 space-y-0 py-5 flex">
+            <input type="text"
+                class="w-6/12 pl-10 pr-4 py-2 text-gray-700 bg-white rounded-md focus:border-grey-200 dark:focus:border-grey-200 focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Search for names">
+            <span class="absolute align-center py-2 px-2"><img height="25" width="25" src="{{ asset('img/search-white.png') }}"
+                    alt="検索"></span>
         </div>
-        <div class="mx-5 w-full">
-          <h1 class="text-xl py-10">ユーザー一覧</h1>
-            <div class="block mt-8 space-y-3 space-y-0 py-5 flex">
-                <input type="text"
-                    class="w-1/6 px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
-                    placeholder="Search for names">
+        <div class="justify-center overflow-auto">
+            <table class="table pt-5 h-5/6">
+                <thead class="bg-lightblue-200">
+                    <tr>
+                        <th class="w-1/8 px-4 py-2 text-xs">登録名</th>
+                        <th class="w-1/8 px-4 py-2 text-xs">会社名</th>
+                        <th class="w-1/4 px-4 py-2 text-xs">メールアドレス</th>
+                        <th class="w-1/8 px-4 py-2 text-xs">被通報回数</th>
+                        <th class="w-2/8 px-4 py-2 text-xs">マッチ数</th>
+                        <th class="w-1/8 px-4 py-2 text-xs"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr class="bg-white">
+                            <td class="border-b px-4 py-2 text-sm">{{ $user->name }}</td>
+                            <td class="border-b px-4 py-2 text-sm">{{ $user->company }}</td>
+                            <td class="border-b px-4 py-2 text-sm">{{ $user->email }}</td>
+                            <td class="border-b px-4 py-2 text-sm"></td>
+                            <td class="border-b px-4 py-2 text-sm"></td>
+                            <td class="border-b px-4 py-2 text-sm text-center"><button
+                                    class="bg-red-500 px-2 py-1 rounded text-white">アカウント停止</button></td>
+                        </tr>
+                    @endforeach
 
-                <button
-                    class="p-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-800 rounded-lg w-auto mx-4 hover:bg-blue-400 focus:outline-none focus:bg-blue-400">
-                    <img src="{{ asset('img/search.svg') }}" alt="検索">
-                </button>
-            </div>
-            <table class="table-fixed justify-center pt-5 justify-center">
-              <thead class="bg-lightblue-200">
-                <tr>
-                  <th class="w-1/6 px-4 py-2 text-xs">登録名</th>
-                  <th class="w-1/6 px-4 py-2 text-xs">会社名</th>
-                  <th class="w-1/6 px-4 py-2 text-xs">メールアドレス</th>
-                  <th class="w-1/6 px-4 py-2 text-xs">被通報回数</th>
-                  <th class="w-1/6 px-4 py-2 text-xs">マッチ数</th>
-                  <th class="w-1/6 px-4 py-2 text-xs"></th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($users as $user) 
-                {{-- 何も出力されない --}}
-                <tr>
-                  <td class="border-b px-4 py-2 text-sm">{{ $user->name }}</td>
-                  <td class="border-b px-4 py-2 text-sm">{{ $user->company }}</td>
-                  <td class="border-b px-4 py-2 text-sm">{{ $user->email }}</td>
-                  <td class="border-b px-4 py-2 text-sm"></td>
-                  <td class="border-b px-4 py-2 text-sm"></td>
-                  <td class="border-b px-4 py-2 text-sm"><button>アカウント停止</button></td>
-                </tr>
-                @endforeach
-
-              </tbody>
+                </tbody>
             </table>
         </div>
     </div>
