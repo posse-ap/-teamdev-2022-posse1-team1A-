4576ac3cb91acdf020d9d4c6615567e03434aaf7
Haruka Miki <haru.m1k1@keio.jp>
2022-04-29 16:06:50 +0900

    管理者サイドバーをrouteで

--- project/routes/web.php
+++ project/routes/web.php
@@ -25,10 +25,10 @@ Route::post('/', 'App\Http\Controllers\UserController@search')->name('user_searc
 
 Route::get('/search/{keyword?}', 'App\Http\Controllers\UserController@result')->name('user_result');
 
-Route::get('/admin/userlist', 'App\Http\Controllers\AdminController@userlist');
+Route::get('/admin/userlist', 'App\Http\Controllers\AdminController@userlist')->name('admin_userlist');
 
 Route::get('/ticket', 'App\Http\Controllers\UserController@ticket')->name('user_ticket');
 
 Route::get('/terms-of-service', function () {
     return view('user.terms-of-service');
-});
+})->name('terms_of_service');
