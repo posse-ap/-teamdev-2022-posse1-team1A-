47787e6e00d23006bfc6e870a16f7fb363001037
Haruka Miki <haru.m1k1@keio.jp>
2022-04-29 15:09:59 +0900

    レビュー修正

--- project/app/Http/Controllers/AdminController.php
+++ project/app/Http/Controllers/AdminController.php
@@ -9,9 +9,7 @@ class AdminController extends Controller
 {
     public function userlist()
     {
-        // $users = User::All();
         $users = User::paginate(10);
-        // dd($users);
         return view('admin.user-list', compact('users'));
     }
 }
