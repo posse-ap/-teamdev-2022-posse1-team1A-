23a95ad5868e06f805c3c725140e53272080d662
Haruka Miki <haru.m1k1@keio.jp>
2022-05-03 20:03:31 +0900

    日程調整モーダルコメントアウト解除

--- project/resources/views/chat/main.blade.php
+++ project/resources/views/chat/main.blade.php
@@ -106,7 +106,31 @@
                 </form>
                 <div class="flex items-center ">
                 </div>
-
+                <div id="faq_csv_modal_window">
+                  {{-- 日程調整モーダルウィンドウ --}}
+                  @include('components.modal_window')
+                  @section('modal_window')
+                      <div id="modal_open">
+                          <main id="modal_main" class="flex flex-col text-center p-8">
+                            <div class="mb-5 font-bold text-lg">相談日程の登録</div>
+                            <div>
+                              <div class="text-left mb-3">相談日程</div>
+                                <form action="" method="post" class="bg-gray-100 rounded-md mb-5">
+                                  <input type="date" name="" id="" class="bg-gray-100 text-left">
+                                </form>
+                              </label>
+                            </div>
+                            <button class="bg-indigo-400 hover:bg-blue-700 text-white font-bold py-2 rounded  w-64 mx-auto mb-5">
+                              チケットを1枚消費して登録
+                            </button>
+                            <p class="text-xs">※日程を登録するとチケットが1枚消費されます。</p>
+                            <p class="text-xs">※ 日程の再調整やキャンセルを繰り返すと、アカウントが停止される可能性があります。</p>
+                          </main>
+                      </div>
+                  @endsection
+                  @yield('modal_window')
+                </div>
+                <div id="faq_csv_modal_window">
                   {{-- 通話開始モーダルウィンドウ --}}
                   {{-- TODO:別の日程調整モーダルとの出し入れしたい --}}
                   @include('components.modal_window')
