f2cca0415dec09047aecad9815720124ad4de081
Haruka Miki <haru.m1k1@keio.jp>
2022-05-04 21:41:43 +0900

    スマホ版font-size下げる

--- project/resources/views/components/modals/call-start.blade.php
+++ project/resources/views/components/modals/call-start.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col text-center py-12">
-    <div class="text-left my-12 text-center pb-3 text-lg">通話を開始します。<br class="md:hidden block">よろしいですか？</div>
+    <div class="text-left my-12 text-center pb-3 md:text-xl text-base">通話を開始します。<br class="md:hidden block">よろしいですか？</div>
     <div class="flex">
         <button class="mr-5 bg-indigo-400 hover:bg-blue-700 text-white font-bold py-2 rounded w-64 mx-auto mb-5">
             通話
