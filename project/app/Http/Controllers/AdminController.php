c6fd16b613c27247959194de06aa6a84d69ea93f
Haruka Miki <haru.m1k1@keio.jp>
2022-05-10 18:52:45 +0900

    指標一覧バック実装完了

--- project/app/Http/Controllers/AdminController.php
+++ project/app/Http/Controllers/AdminController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\AccountStatus;
+use App\Models\Calling;
+use App\Models\InterviewSchedule;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $total_count_users = User::count(); //総会員数
+        $total_count_matched = User::selectRaw('SUM(match_count)/2 as total_count_matched')->first();// 総マッチ数
+        $total_count_call = Calling::count(); // 総通話数
+        $avg_calling_time = Calling::selectRaw('AVG(calling_time) average_calling_time')->first(); // 平均通話時間
+        $total_count_cancelled = InterviewSchedule::where('schedule_status_id',2)->count();// 日程調整失敗数
+        return view('admin.index', compact('total_count_users','total_count_matched','total_count_call', 'avg_calling_time','total_count_cancelled'));
     }
 
     public function userlist()
