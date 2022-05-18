aee4ab83444533ef98735ba763833de7d04da072
tomoharuyuyama <tomoharuyuyama@gmail.com>
2022-05-18 15:03:09 +0900

    buttonタグをpタグに修正

--- project/resources/views/user/info.blade.php
+++ project/resources/views/user/info.blade.php
@@ -67,9 +67,9 @@
                 </div>
                 <a href="{{ route('user_edit') }}"
                     class="block bg-lightblue-500 hover:bg-blue-300 text-white sm:text-base text-xs py-1 px-4 rounded mx-auto mb-5 w-60 text-center">
-                    <button>
+                    <p>
                         プロフィールを編集する
-                    </button>
+                    </p>
                 </a>
                 <a href="{{ route('user_withdrawal') }}"
                     class="block bg-red-400 hover:bg-red-500 text-white sm:text-base text-xs py-1 px-4 rounded mx-auto w-60 text-center">
