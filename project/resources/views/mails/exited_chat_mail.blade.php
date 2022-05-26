b58076b6f39b9e0b33dba449aedf5030bc8d8c5c
Haruka Miki <haru.m1k1@keio.jp>
2022-05-26 20:29:51 +0900

    メールにインデント

--- project/resources/views/mails/exited_chat_mail.blade.php
+++ project/resources/views/mails/exited_chat_mail.blade.php
@@ -1,12 +1,12 @@
-{{$receiver->nickname}} 様
-
-Anovey (アノベイ) をご利用いただき誠にありがとうございます。
-当サービスにて{{$partner->nickname}} 様とのチャットを退出されました。
-これより、{{$partner->nickname}} 様とのチャットはチャット一覧画面より削除されました。
-
-今後ともAnovey をよろしくお願いいたします。
-
-■Anovey HP
-{{ route('user_index') }}
-
-Anovey運営
+{{$receiver->nickname}} 様<br>
+<br>
+Anovey (アノベイ) をご利用いただき誠にありがとうございます。<br>
+当サービスにて{{$partner->nickname}} 様とのチャットを退出されました。<br>
+これより、{{$partner->nickname}} 様とのチャットはチャット一覧画面より削除されました。<br>
+<br>
+今後ともAnovey をよろしくお願いいたします。<br>
+<br>
+■Anovey HP<br>
+{{ route('user_index') }}<br>
+<br>
+Anovey運営<br>
