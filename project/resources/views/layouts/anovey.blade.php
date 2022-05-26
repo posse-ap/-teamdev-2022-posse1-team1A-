13481b04a053ff2d3cc35f1768a689f2afc6334f
Haruka Miki <haru.m1k1@keio.jp>
2022-05-26 14:59:16 +0900

    faviconの設定

--- project/resources/views/layouts/anovey.blade.php
+++ project/resources/views/layouts/anovey.blade.php
@@ -19,7 +19,10 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         @stack('styles')
-
+        
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
+        
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         @stack('scripts')
