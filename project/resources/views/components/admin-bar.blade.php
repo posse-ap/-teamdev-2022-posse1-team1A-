889465d6a93bb9c81599bb9ccab480a7492df2df
Haruka Miki <haru.m1k1@keio.jp>
2022-05-29 14:29:01 +0900

    管理者メニューバーのロゴ押したら各種指標へ遷移

--- project/resources/views/components/admin-bar.blade.php
+++ project/resources/views/components/admin-bar.blade.php
@@ -1,9 +1,9 @@
 <div class="w-1/5 bg-blue-900 min-h-screen">
     <ul class="flex-grow">
-        <div class="w-15 h-15 mt-3 flex mx-5">
+        <a href="{{ route('admin.index') }}" class="w-15 h-15 mt-3 flex mx-5">
             <img src="{{ asset('img/logo-white.png') }}" alt="logo-black.png">
             <p class="ml-0 text-3xl text-white flex items-center font-bold">Anovey</p>
-        </div>
+        </a>
         <li class="mx-5 border-b"><a class="block py-5 pl-2 rounded text-white" href="#">管理者ページ</a>
         </li>
         <li><a class="block mx-5 py-5 pl-2 mt-10 hover:bg-gray-300 hover:text-gray-800 rounded text-white text-xs"
